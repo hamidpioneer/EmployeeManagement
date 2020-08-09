98e1e1d97cba022188b8ed9282aff91f6e27877b
Hamid Pioneer <hamidpioneer@yahoo.com>
2020-08-10 01:17:40 +0600

    End 20: MVC - Controller

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace EmployeeManagement.Controllers
 {
     public class HomeController : Controller
     {
-        private IEmployeeRepository _employeeRepository;
+        private readonly IEmployeeRepository _employeeRepository;
 
         public HomeController(IEmployeeRepository employeeRepository)
         {
@@ -19,7 +19,12 @@ namespace EmployeeManagement.Controllers
         public string Index()
         {
             return _employeeRepository.GetEmployee(2).Name;
-
+        }
+        public ViewResult Details()
+        {
+            Employee model = _employeeRepository.GetEmployee(1);
+            //return new ObjectResult(model);
+            return View(model);
         }
     }
 }

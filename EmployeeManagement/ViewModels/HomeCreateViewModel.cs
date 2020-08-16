176c270da87471fdb2e8512fb5c4582c081472d3
Hamid Pioneer <hamidpioneer@yahoo.com>
2020-08-17 02:57:10 +0600

    End 53: File Upload

---
+++ EmployeeManagement/ViewModels/HomeCreateViewModel.cs
@@ -0,0 +1,28 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class HomeCreateViewModel
+    {
+        [Required]
+        [MaxLength(50, ErrorMessage = "Name can nto exceed 50 characters!")]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        //[DisplayName("Office Email")]
+        [Display(Name = "Office Email")]
+        public string Email { get; set; }
+
+        [Required]
+        public Dept? Department { get; set; }
+
+        public IFormFile Photo { get; set; }
+    }
+}

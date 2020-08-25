09b2339bee0e8e279e614463abdda019db481ad3
Hamid Pioneer <hamidpioneer@yahoo.com>
2020-08-26 02:42:18 +0600

    End 62: Logging Exception with ILogger Interface of type of Controller

--- EmployeeManagement/Controllers/ErrorController.cs
+++ EmployeeManagement/Controllers/ErrorController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,12 @@ namespace EmployeeManagement.Controllers
 {
     public class ErrorController : Controller
     {
+        private readonly ILogger<ErrorController> logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            this.logger = logger;
+        }
 
         [Route("Error/{statusCode}")]
         public IActionResult HttpStatusCodeHandler(int statusCode)
@@ -43,6 +50,9 @@ namespace EmployeeManagement.Controllers
             ViewBag.ExceptionStackTrace = exceptionHandlerPathFeature.Error.StackTrace;
             ViewBag.ExceptionInnerException = exceptionHandlerPathFeature.Error.InnerException;
             ViewBag.ExceptionSource = exceptionHandlerPathFeature.Error.Source;
+
+            logger.LogWarning($"This is an exception on path = {exceptionHandlerPathFeature.Path} &" +
+                              $" Error = {exceptionHandlerPathFeature.Error}");
 
             return View("Error");
         }
